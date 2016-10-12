3610d06bc01e90f31eb461ab1f26f7b68f9e0f2e
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-10-12 21:35:33 +0200

    set up http server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import lawyerProvider from './provider/lawyers.js';
+//import lawyerProvider from './provider/lawyers.js';
 
-lawyerProvider().getLawyer().then( data => console.log(data)).catch(err => console.error(err));
+//lawyerProvider().getLawyer().then( data => console.log(data)).catch(err => console.error(err));
+//
+
+import restify from 'restify';
+
+const server = restify.createServer();
+
+server.get('/user/:login',
+
+server.get(/.*/, restify.serveStatic({
+    directory: 'public',
+    default: 'index.html'
+}));
+server.listen(8180, () => console.log('Server started'));
+
+
